6e7a9dc1f569a1569590ae29102959084f0b4a89
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-26 20:05:18 +0700

    insert include.php & foundation

--- views/include.php
+++ views/include.php
@@ -1,5 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
+<script type="text/javascript" src="js/foundation.min.js?v=1001"></script>
+<link rel="stylesheet" href="css/foundation.min.css?v=1001">
 <link rel="stylesheet" href="css/style.css">
 <!-- <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"> -->
