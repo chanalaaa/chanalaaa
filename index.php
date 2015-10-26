127f334ab85c015fbe12c9ca0bf4fbf714aa69de
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-26 18:50:24 +0700

    initial project

---
+++ index.php
@@ -0,0 +1,19 @@
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <title></title>
+        <?php include("views/include.php"); ?>
+    </head>
+
+    <body>
+        <div id="wrapper">
+            <?php //include("views/header.php"); ?>
+                <?php //include("views/content.php"); ?>
+                    <?php //include("views/footer.php"); ?>
+                        <script type="text/javascript" src="js/main.js"></script>
+        </div>
+    </body>
+
+    </html>
