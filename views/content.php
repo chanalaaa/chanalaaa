436c1e74fbff44a8e39a0d498a6586569dbab8d2
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-29 20:17:35 +0700

    finish .box-w

--- views/content.php
+++ views/content.php
@@ -1,11 +1,13 @@
 <!-- CONTENT ************************** -->
-  <div class="section main">
-f
-  </div>
+<div class="section main">
+    <div class="box-bg">
+        <div class="box-main"></div>
+        <div class="box-w_center"></div>
+        <div class="box-w_top"></div>
+        <div class="box-w_right"></div>
+    </div>
+</div>
 <!-- END CONTENT ************************** -->
-
 <!-- SCRIPT ************************** -->
 <script type="text/javascript">
-
 </script>
-
